f13a6ad2a5b2c1648ac938dde2f806eb1b8d4bc9
sumitkd16 <145481925+sumitkd16@users.noreply.github.com>
2025-08-23 09:11:44 +0530

    lasttt

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -43,7 +43,7 @@ const About = () => {
                     </p>
                     {/* Resume Button */}
                     <a
-                        href="https://drive.google.com/file/d/1i4yRo-v_XBXGQsoxDmBLxzDWeKob2gBe/view?usp=sharing"
+                        href="https://drive.google.com/file/d/1JuFo7XuAlH2ZC3R4HdHDol9ztDoV-anB/view?usp=sharing"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
